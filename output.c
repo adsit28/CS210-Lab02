0e58fa4c092d0252189e4ca123241d4fc3a50ba5
Adrian de Freitas <Adefreit@users.noreply.github.com>
2022-07-08 10:43:53 -0600

    Initial Upload

---
+++ output.c
@@ -0,0 +1,12 @@
+/* lab02.c
+ * ===========================================================
+ * Name: 
+ * Section:
+ * Project: Lab 2 - Output
+ * Purpose: Learn C basics
+ */
+
+// What do you need to #include to use the printf/scanf functions?
+
+
+// Put Your Main Function Here
